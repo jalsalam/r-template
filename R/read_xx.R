cb53ce8065f923cf4b1fd0e10b9e23d5935c8446
Jameel Alsalam <jalsalam@gmail.com>
2017-01-26 21:03:23 -0500

    defined some structure to the read data functions (e.g., single table, plus map function)

--- R/read_xx.R
+++ R/read_xx.R
@@ -1,3 +1,12 @@
 # read_xx.R
 # Reads input data from xx
 
+read_xx_table <- function(table) {
+  #function defines how to read a single table
+  
+}
+
+read_xx_set <- function(set_of_tables) {
+  #function calls read_xx_table for each member of set_of_tables
+  
+}
